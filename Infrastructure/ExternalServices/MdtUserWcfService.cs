d6ad2fb8b877a4c2ef9a2fb2819efe00ae706311
Amr Abdeltawab <Amr.Abdeltawab@softec.ai>
2025-04-29 17:29:35 +0400

    fix issue

--- Infrastructure/ExternalServices/MdtUserWcfService.cs
+++ Infrastructure/ExternalServices/MdtUserWcfService.cs
@@ -1,29 +1,27 @@
 using System.ServiceModel;
 using System.Threading.Tasks;
 using Application.Abstractions;
+using Saaed360Modern.Infrastructure.ExternalServices.MDTReportServiceRef;
+using Saaed360Modern.Infrastructure.ExternalServices.MdtUserServiceRef;
 
 namespace Saaed360Modern.Infrastructure.ExternalServices;
 
 public class MdtUserWcfService : IMdtUserWcfService
 {
-    private readonly MdtUserServiceRef.IMdtUserServiceChannel _client;
+    private readonly MdtUserServiceClient _client;
 
     public MdtUserWcfService()
     {
         var binding = new BasicHttpBinding
         {
-            MaxReceivedMessageSize = 2147483647,
-            Security = new BasicHttpSecurity
-            {
-                Mode = BasicHttpSecurityMode.Transport
-            }
+            Security = new BasicHttpSecurity { Mode = BasicHttpSecurityMode.Transport },
+            MaxReceivedMessageSize = 1024 * 1024 * 20 // 20 MB – tune as needed
         };
 
+        // TODO: Read endpoint address from configuration
         var endpoint = new EndpointAddress("https://saaed360test.saaed.ae/Saaed360WCF/MdtUserService.svc");
-        _client = new ChannelFactory<MdtUserServiceRef.IMdtUserServiceChannel>(binding, endpoint).CreateChannel();
+        _client = new MdtUserServiceClient(binding, endpoint);
     }
-
-    
 
 
 } 
